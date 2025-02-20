dc27cb44313adf663be796578e510eba11c10238
popples <klauspopp@gmx.de>
2025-02-20 17:06:07 +0000

    basically working

--- src/main.c
+++ src/main.c
@@ -3,33 +3,56 @@
 #include <stdlib.h>
 #include <string.h>
 
+// only packet3 supported
+// ODR must be same for accel and gyro (gyro ODR ignored)
+// FIFO watermark in records only
+// FIFO count & data in little endian only
+// timestamp res 1us
+
 // Registers to simulate
 #define ICM426XX_INT_STATUS 0x2D  // 8 bits
 #define ICM426XX_FIFO_COUNTH 0x2E   // 16 bits
 #define ICM426XX_FIFO_DATA 0x30 // any length
-#define ICM426XX_GYRO_CONFIG0 0x4F // 8 bits  - ODR
-#define ICM426XX_ACCEL_CONFIG0 0x50 // 8 bits - ODR
+#define ICM426XX_ACCEL_CONFIG0 0x50 // 8 bits - ODR 
+#define ICM426XX_ACCEL_CONFIG2 0x60 // 8 bits - FIFO watermark
+#define ICM426XX_ACCEL_CONFIG3 0x61 // 8 bits - FIFO watermark MSB
 #define ICM426XX_PWR_MGMT0 0x4E // 8 bits - enable/disable
-#define ICM426XX_FIFO_CONFIG1 0x5F // 8 bits - fifo packet
 #define ICM426XX_REG_BANK_SEL 0x76 // 8 bits - select bank
 typedef struct
 {
   pin_t cs_pin;
   uint32_t spi;
   uint8_t spi_buffer[1];
-  uint32_t spi_data_from_host[3]; // reg, data0, data1
+  uint8_t spi_data_from_host[3]; // reg, data0, data1
   uint32_t spi_data_from_host_idx;
   uint8_t reg_addr;
   uint8_t selected_bank;
   bool is_write;
   uint16_t regs[0x80]; // bank 0 only
+  uint8_t fifo[2048];
+  uint16_t fifo_read_idx;
+  uint16_t fifo_write_idx;
+  uint16_t fifo_count;
   timer_t sample_timer;
   uint32_t accel_amplitude_attr;
   uint32_t gyro_amplitude_attr;
 } chip_state_t;
 
+typedef struct __attribute__((__packed__)){
+  uint8_t header;
+  uint16_t accel_x;
+  uint16_t accel_y;
+  uint16_t accel_z;
+  uint16_t gyro_x;
+  uint16_t gyro_y;
+  uint16_t gyro_z;
+  uint8_t temp;
+  uint16_t timestamp;
+} packet3_t;
+
 static void chip_pin_change(void *user_data, pin_t pin, uint32_t value);
 static void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count);
+static uint32_t odr_to_millihz(uint8_t odr);
 
 static void dump_buffer(uint8_t *buffer, size_t size)
 {
@@ -40,14 +63,73 @@ static void dump_buffer(uint8_t *buffer, size_t size)
   printf("\n");
 }
 
+static void update_fifo_count(chip_state_t *chip, int16_t count)
+{
+  chip->fifo_count += count;
+  printf("FIFO count %d\n", chip->fifo_count);
+
+  uint16_t records = chip->fifo_count/sizeof(packet3_t);
+  chip->regs[ICM426XX_FIFO_COUNTH] = records & 0xff;
+  chip->regs[ICM426XX_FIFO_COUNTH+1] = (records >> 8) & 0xff;
+}
+
+static void put_fifo_data(chip_state_t *chip, uint8_t *buffer, size_t size)
+{
+  size_t update_size = size;
+  if(chip->fifo_count + size > sizeof(chip->fifo)){
+    // discard oldest data
+    chip->fifo_read_idx = (chip->fifo_read_idx + size) % sizeof(chip->fifo);
+    update_size = 0;
+
+    printf("FIFO overflow\n");
+    chip->regs[ICM426XX_INT_STATUS] |= 0x02; // FIFO full
+  }
+  memcpy(chip->fifo + chip->fifo_write_idx, buffer, size);
+  chip->fifo_write_idx = (chip->fifo_write_idx + size) % sizeof(chip->fifo);
+  update_fifo_count(chip, update_size);
+
+  uint16_t fifo_wm = chip->regs[ICM426XX_ACCEL_CONFIG2] | (chip->regs[ICM426XX_ACCEL_CONFIG3]<<8);
+  if(chip->fifo_count/sizeof(packet3_t) >= fifo_wm){
+    printf("FIFO watermark\n");
+    chip->regs[ICM426XX_INT_STATUS] |= 0x04; // FIFO watermark
+  }
+}
+
+static uint8_t read_fifo_reg(chip_state_t *chip)
+{
+  if(chip->fifo_count == 0){
+    printf("FIFO empty host_idx=%d!\n", chip->spi_data_from_host_idx);
+    return 0;
+  }
+  uint8_t data = chip->fifo[chip->fifo_read_idx];
+  chip->fifo_read_idx = (chip->fifo_read_idx + 1) % sizeof(chip->fifo);
+  update_fifo_count(chip, -1);
+  return data;
+}
 
 
-// static void on_dcdc_timer(void *user_data)
-// {
-//   chip_state_t *chip = user_data;
-//   printf("Enabled DCDC\n");
-//   chip->regs[REG_STATUS] &= ~STATUS_SEC_FAIL;
-// }
+static void on_sample_timer(void *user_data)
+{
+  chip_state_t *chip = user_data;
+  if(!(chip->regs[ICM426XX_PWR_MGMT0] & 0x02)){
+    // chip not turned on
+    return;
+  }
+
+  packet3_t packet = {
+      .header = 0x03,
+      .accel_x = 0x1234,
+      .accel_y = 0x5678,
+      .accel_z = 0x9abc,
+      .gyro_x = 0xdef0,
+      .gyro_y = 0x1234,
+      .gyro_z = 0x5678,
+      .temp = 0x12,
+      .timestamp = (get_sim_nanos() / 1000) & 0xffff,
+  };
+  printf("Sample timer pkt size %ld\n", sizeof(packet));
+  put_fifo_data(chip, (uint8_t *)&packet, sizeof(packet));
+}
 
 void chip_init(void)
 {
@@ -57,10 +139,10 @@ void chip_init(void)
 
   chip->cs_pin = pin_init("CS", INPUT_PULLUP);
 
-  // chip->dcdc_timer = timer_init(&(timer_config_t){
-  //     .user_data = chip,
-  //     .callback = on_dcdc_timer,
-  // });
+  chip->sample_timer = timer_init(&(timer_config_t){
+      .user_data = chip,
+      .callback = on_sample_timer,
+  });
 
   const pin_watch_config_t watch_config = {
       .edge = BOTH,
@@ -83,15 +165,23 @@ void chip_init(void)
 
 static uint8_t read_reg(chip_state_t *chip, uint8_t reg)
 {
+  uint8_t data = 0;
   if(chip->selected_bank != 0){
     return 0; // no other banks supported
   }
   if( reg != ICM426XX_FIFO_DATA){
-    return chip->regs[reg]; 
+    data = chip->regs[reg];
+    switch(reg){
+    case ICM426XX_INT_STATUS:
+      //printf("Reading INT_STATUS\n");
+      chip->regs[ICM426XX_INT_STATUS] = 0; // clear interrupt
+      break;
+    }
   } else {
     printf("Reading FIFO data\n");
-    return 0xff; // TODO:
+    data = read_fifo_reg(chip);
   }
+  return data;
 }
 
 static void write_reg(chip_state_t *chip, uint8_t reg, uint8_t value)
@@ -99,9 +189,18 @@ static void write_reg(chip_state_t *chip, uint8_t reg, uint8_t value)
   if(chip->selected_bank != 0){
     return; // no other banks supported
   }
-  if( reg == ICM426XX_REG_BANK_SEL){
+  switch(reg){
+  case ICM426XX_REG_BANK_SEL:
     printf("Select bank %d\n", value);
     chip->selected_bank = value;
+    return;
+  case ICM426XX_ACCEL_CONFIG0:
+    printf("Setting accel ODR to %d\n", value);
+    chip->regs[reg] = value;
+    uint64_t tv = 1E12/odr_to_millihz(value&0xf);
+    printf("Setting timer to %lld\n", tv);
+    timer_stop(chip->sample_timer);
+    timer_start_ns(chip->sample_timer, tv, true);
     return;
   }
   printf("Writing reg %02x with value %02x\n", reg, value);
@@ -160,24 +259,26 @@ void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count)
     return;
   }
 
-  if( chip->spi_data_from_host_idx >= sizeof(chip->spi_data_from_host) ){
-    printf("Received too many %d bytes\n", count);
-    return;
-  }
   uint8_t data = buffer[0];
-  printf("Received byte %02x\n", data);
-  chip->spi_data_from_host[chip->spi_data_from_host_idx] = data;
-
+  //printf("Received byte %02x idx %d\n", data, chip->spi_data_from_host_idx);
+  
+  if(chip->spi_data_from_host_idx < sizeof(chip->spi_data_from_host)){
+    chip->spi_data_from_host[chip->spi_data_from_host_idx] = data;
+  }
   if(chip->spi_data_from_host_idx==0){
     chip->reg_addr = data & 0x7f;
     chip->is_write = (data & 0x80) == 0;
   } else {
     if( chip->reg_addr != ICM426XX_FIFO_DATA){
-      chip->reg_addr++;
+      if(chip->spi_data_from_host_idx>1){
+        chip->reg_addr++;
+      }
     }
   }
+  //printf("Reg addr %02x write=%d\n", chip->reg_addr, chip->is_write);
   if(!chip->is_write){
     buffer[0] = read_reg(chip, chip->reg_addr);
+    //printf("Sending byte %02x\n", buffer[0]);
   } else {
     buffer[0] = 0;
   }
@@ -187,6 +288,46 @@ void chip_spi_done(void *user_data, uint8_t *buffer, uint32_t count)
   {
     // Continue with the next character
     spi_start(chip->spi, chip->spi_buffer, 1);
+  } else {
+    printf("CS high\n");
   }
 }
 
+static uint32_t odr_to_millihz(uint8_t odr)
+{
+  switch (odr)
+  {
+  case 1:
+    return 32000*1000;
+  case 2:
+    return 16000*1000;
+  case 3:
+    return 8000*1000;
+  case 4:
+    return 4000*1000;
+  case 5:
+    return 2000*1000;
+  case 6:
+    return 1000*1000;
+  case 7:
+    return 200*1000;  
+  case 8:
+    return 100*1000;
+  case 9:
+    return 50*1000;
+  case 10:
+    return 25*1000;
+  case 11:
+    return 12500;
+  case 12:
+    return 6250;
+  case 13:
+    return 3125;
+  case 14:
+    return 1563;
+  case 15:
+    return 500*1000;
+  default:
+    return 0;
+  }
+}
--- test/main/main.ino
+++ test/main/main.ino
@@ -6,11 +6,23 @@
 #define ICM426XX_INT_STATUS 0x2D  // 8 bits
 #define ICM426XX_FIFO_COUNTH 0x2E   // 16 bits
 #define ICM426XX_FIFO_DATA 0x30 // any length
-#define ICM426XX_GYRO_CONFIG0 0x4F // 8 bits  - ODR
-#define ICM426XX_ACCEL_CONFIG0 0x50 // 8 bits - ODR
+#define ICM426XX_ACCEL_CONFIG0 0x50 // 8 bits - ODR 
+#define ICM426XX_ACCEL_CONFIG2 0x60 // 8 bits - FIFO watermark
+#define ICM426XX_ACCEL_CONFIG3 0x61 // 8 bits - FIFO watermark MSB
 #define ICM426XX_PWR_MGMT0 0x4E // 8 bits - enable/disable
-#define ICM426XX_FIFO_CONFIG1 0x5F // 8 bits - fifo packet
 #define ICM426XX_REG_BANK_SEL 0x76 // 8 bits - select bank
+
+typedef struct __attribute__((__packed__)){
+  uint8_t header;
+  uint16_t accel_x;
+  uint16_t accel_y;
+  uint16_t accel_z;
+  uint16_t gyro_x;
+  uint16_t gyro_y;
+  uint16_t gyro_z;
+  uint8_t temp;
+  uint16_t timestamp;
+} packet3_t;
 
 
 void dumpBuffer(uint8_t *buffer, size_t size)
@@ -24,7 +36,7 @@ void dumpBuffer(uint8_t *buffer, size_t size)
 }
 
 
-void spiTransfer(uint8_t *buffer, size_t len)
+void spi_transfer(uint8_t *buffer, size_t len)
 {
   digitalWrite(CS, LOW);
   SPI.begin();
@@ -40,14 +52,14 @@ void write_reg(uint8_t reg, uint8_t value)
   uint8_t buffer[2] = {0};
   buffer[0] = reg;
   buffer[1] = value;
-  spiTransfer(buffer, sizeof(buffer));
+  spi_transfer(buffer, sizeof(buffer));
 }
 
 uint8_t read_reg(uint8_t reg)
 {
   uint8_t buffer[2] = {0};
   buffer[0] = 0x80 | reg;
-  spiTransfer(buffer, sizeof(buffer));
+  spi_transfer(buffer, sizeof(buffer));
   return buffer[1];
 }
 
@@ -56,26 +68,63 @@ uint8_t read_reg16(uint8_t reg)
 {
   uint8_t buffer[3] = {0};
   buffer[0] = 0x80 | reg;
-  spiTransfer(buffer, sizeof(buffer));
+  spi_transfer(buffer, sizeof(buffer));
   return (((uint16_t)buffer[2]) << 8) | buffer[1];
 }
 
-
+// caller shall discard the first byte of the buffer
+void read_fifo(uint8_t *buffer, size_t size)
+{
+  uint8_t reg = ICM426XX_FIFO_DATA;
+  buffer[0] = 0x80 | reg;
+  spi_transfer(buffer, size);
+}
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(CS, OUTPUT);
-
+  Serial.println("Setup");
   write_reg(ICM426XX_REG_BANK_SEL, 0x00);
-  write_reg(ICM426XX_GYRO_CONFIG0, 0xaa);
+  write_reg(ICM426XX_ACCEL_CONFIG0, 0x9); // 50Hz
+  write_reg(ICM426XX_ACCEL_CONFIG2, 0x40); // watermark
+  write_reg(ICM426XX_ACCEL_CONFIG3, 0x0); // watermark MSB
   uint8_t v;
-  v = read_reg(ICM426XX_GYRO_CONFIG0);
-  Serial.print("Gyro config: ");
+  v = read_reg(ICM426XX_ACCEL_CONFIG0);
+  Serial.print("Accel config: ");
   Serial.println(v, HEX);
+
+  write_reg(ICM426XX_PWR_MGMT0, 0xf); // enable
 }
 
+uint8_t buffer[1500];
 void loop()
 {
-  delay(500);
+  static int loop_cnt;
+  delay(100);
+  Serial.println("Loop");
+  loop_cnt++;
+  Serial.println(loop_cnt);
+  uint8_t int_status;
+  uint16_t fifo_count;
+
+  int_status = read_reg(ICM426XX_INT_STATUS);
+  Serial.print("INT_STATUS: ");
+  Serial.println(int_status, HEX);
+
+  fifo_count = read_reg16(ICM426XX_FIFO_COUNTH);
+  Serial.print("FIFO count: ");
+  Serial.println(fifo_count, HEX);
+
+  if(int_status & 0x04){
+    Serial.println("FIFO watermark");
+    
+    size_t size = sizeof(packet3_t)*fifo_count+1;
+    if(size < sizeof(buffer)){
+      read_fifo(buffer, size);
+      dumpBuffer(buffer+1, size-1);
+    } else {
+      Serial.println("FIFO too large");
+    }
+  }
 }
